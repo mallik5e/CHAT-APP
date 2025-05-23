a6e56d2a42d22047d796b676fbd568815ff8cafb
Mallikarjuna <manumallu81@gmail.com>
2025-05-24 01:33:09 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-      </h1>
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+      </Routes>
     </div>
   )
 }
